3a155a857201149e211c3ae44c6ea361820879fc
tfournet <tfournet@radersolutions.com>
2020-09-10 19:30:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ cp cron/imp_hunter_daily /etc/cron.daily
 chmod a+x /etc/cron.daily/imp_hunter_daily
 
 mkdir -p $installdir
-cp -f bash/$scriptfile $installdir
+cp -f sh/$scriptfile $installdir
 chmod a+x $installdir/$scriptfile
 
 echo "Initial setup complete. Add domain(s) to monitor to $domainlist and run $installdir/$scriptfile to run manually."
