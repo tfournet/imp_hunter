b57b0dd01de3ab2e23abbf524261fb1679b893da
tfournet <tfournet@radersolutions.com>
2020-09-10 19:23:27 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Initial version 2020-09-10 by Tim Fournet <tfournet@tfour.net>
+# Updated 2020-09-10 by Tim Fournet <tfournet@tfour.net>
+
+domainlist="/etc/perch/domains.txt"
+installdir="/opt/imp_hunter"
+scriptfile="imp_hunter.sh"
+
+echo"Making sure required packages are installed (docker, bind-utils)"
+yum -y install docker bind-utils 
+
+echo "Creating domain list file $domainlist"
+touch $domainlist
+
+echo "Setting up Daily Cron Job"
+cp cron/imp_hunter_daily /etc/cron.daily
+chmod a+x /etc/cron.daily/imp_hunter_daily
+
+cp -f bash/$scriptfile $installdir
+chmod a+x $installdir/$scriptfile
+
+echo "Initial setup complete. Add domain(s) to monitor to $domainlist and run $installdir/$scriptfile to run manually."
+echo "System will automatically scan nightly"
+
